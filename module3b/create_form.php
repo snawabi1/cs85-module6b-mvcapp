4aff283914f322a770b30cd3e50ad8ab3e566e5d
Sophia N <nawabi_sophia01@student.smc.edu>
2025-07-05 13:13:14 -0700

    Final testing and reflection comments

--- module3b/create_form.php
+++ module3b/create_form.php
@@ -1,4 +1,16 @@
 <?php
+// Full Name: Sophia Nawabi
+// GitHub Repo: https://github.com/snawabi1/cs85-module3b-createform
+
+// Output prediction: I expect a thank-you message showing name, topic, and email.
+// $_POST contents: fullname, email, topic, message
+// Reflections: I learned how to sanitize/validate form data using PHP.
+// I had to fix a bug where the file was wrong in file during testing 3/ i had it in 3b.
+// Surprises: Also going through submission instructions once everything was done I noticed I needed to commit
+// as I went along so I moved everything to another file before I was going to send the entire file and added chunks
+// this caused a some delay as it was done at last minute. 
+// Insights: future note to self: read the sumbission instructions first before assignment instructions.
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Sanitize inputs
     $name = htmlspecialchars(trim($_POST['fullname'] ?? ''));
