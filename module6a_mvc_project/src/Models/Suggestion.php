9dfb622eb80c1bf12cc9ab6fae7cdb6b8ec142c8
Sophia N <nawabi_sophia01@student.smc.edu>
2025-07-13 17:28:59 -0700

    Initial commit: Add Suggestion model with display method

---
+++ module6a_mvc_project/src/Models/Suggestion.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+class Suggestion {
+    public $text;
+    public $category;
+    public $priority;
+
+    public function __construct($text, $category = "General", $priority = "Low") {
+        $this->text = $text;
+        $this->category = $category;
+        $this->priority = $priority;
+    }
+
+    public function display() {
+        return "{$this->text} ({$this->category} - {$this->priority})";
+    }
+}
+
+
